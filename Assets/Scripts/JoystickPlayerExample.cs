61d3ed826e77c490b9c0cafd62ddf6c09f81bea2
xuyenlon87 <xuyenlon87@gmail.com>
2023-08-04 21:05:33 +0700

    21h 5p

---
+++ Assets/Scripts/JoystickPlayerExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public DynamicJoystick dynamicJoystick;
+    public Rigidbody rb;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * dynamicJoystick.Vertical + Vector3.right * dynamicJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
