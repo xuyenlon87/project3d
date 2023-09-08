e31c2d0477df680c268876b87ce29edd2e041a0e
ducnguyen <mducnguyen1511@gmail.com>
2023-09-08 21:01:40 +0700

    joystick+camera

--- Assets/Scripts/JoystickPlayerExample.cs
+++ Assets/Scripts/JoystickPlayerExample.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class JoystickPlayerExample : MonoBehaviour
 {
     public float speed;
-    public DynamicJoystick dynamicJoystick;
+    public FloatingJoystick floatingJoystick;
     public Rigidbody rb;
 
     public void FixedUpdate()
     {
-        Vector3 direction = Vector3.forward * dynamicJoystick.Vertical + Vector3.right * dynamicJoystick.Horizontal;
+        Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
         rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
     }
 }
