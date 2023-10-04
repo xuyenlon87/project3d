faf1cbd8177da1d854612e8ab88e34cddeaf833b
Linh Đặng Vũ <xuyenlon87@gmail.com>
2023-10-04 11:03:37 +0700

    11h 4/10

--- Assets/Scripts/LookPlayer.cs
+++ Assets/Scripts/LookPlayer.cs
@@ -6,21 +6,39 @@ public class LookPlayer : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
-    private Vector3 offset;
+    public Vector3 offset;
+    private float speed = 2f;
+    private bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
-        if(target)
-        offset = transform.position - target.position;
+        if (target != null)
+        {
+            StartCoroutine(MoveCamera());
+        }
+    }
+
+    private IEnumerator MoveCamera()
+    {
+        isMoving = true;
+        Vector3 targetPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+        while(Vector3.Distance(transform.position, target.position) > 1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+            yield return null;
+        }
+        isMoving = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target)
+        if (target != null)
         {
-            Vector3 newPos = target.position + offset;
-            transform.position = newPos;
+            if (!isMoving)
+            {
+                transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+            }
         }
     }
 }
